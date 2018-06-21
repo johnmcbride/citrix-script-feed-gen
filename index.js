ff5fb511fcc6b2d28ce10cc06b7008040ae8274a
John McBride <john@johnmcbride.me>
2018-06-21 14:39:21 -0400

    Updated command line args to remove --type

--- index.js
+++ index.js
@@ -4,30 +4,16 @@ const cmdr = require('commander');
 const fs = require('fs');
 const path = require('path');
 const admZip = require('adm-zip');
-// const feed = require('feed');
 const feed = require('rss');
 const chalk = require('chalk');
 const toXml = require('jstoxml');
+const url = require('url').URL;
 
-// cmdr.option('-c --create','Parses through a specified directory and creates a feed.xml file from the existing Citrix script packages.')
-// .option('-d --directory <path>','Specifies the directory to parse.')
-// .option('-t --type <type>','Specifies the access type (FILE,HTTP')
-// .option('-s --baseurl <base url>','Specifies the base FILE/HTTP URL for the VSIX files')
-// .parse(process.argv);
 
-// console.log(cmdr);
-
-let createFunction = (options) => {
-    //console.log(options);
-    
+let createFunction = (options) => {    
     if (options.directory == null)
     {
         console.log(chalk.red('Please pass in the --directory parameter'));
-        return;
-    }
-    if (options.type == null)
-    {
-        console.log(chalk.red('Please pass in the --type parameter. It should be FILE or HTTP.'));
         return;
     }
     if (options.baseurl == null)
@@ -44,7 +30,7 @@ let createFunction = (options) => {
         return;
     }
 
-    if ( options.type.toLowerCase() == 'file' && options.baseurl == '.')
+    if ( options.baseurl == '.')
     { 
         options.baseurl = path.resolve(options.baseurl);
     }
@@ -68,19 +54,6 @@ let createFunction = (options) => {
             ]
         }
     };
-
-   
-   
-
-    // let feedOptions = {
-    //     title: 'Citrix Script packages',
-    //     description: 'Custom feed for Citrix Script packages',
-    //     link: 'http://developer.citrix.com'
-    // };
-
-    // let vsixFeed = new feed(feedOptions);
-    // fs.writeFileSync('./feed.xml',vsixFeed.xml());
-    // return;
     
     //check to make sure that directory is passed in...
     fs.readdirSync(options.directory).forEach( (file) => {
@@ -96,70 +69,73 @@ let createFunction = (options) => {
 
             zip.getEntries().forEach(function(entry) {
                 var entryName = entry.entryName;
-                console.log(entryName);
-                
+
                 if ( entryName.indexOf('manifest.json') != -1 )
                 {
+                    console.log(`Found manifest file in VSIX (${file})`);
+                    
                     var newVsixItem = {};
                     newVsixItem.item = {};
 
-                    console.log(zip.readAsText(entry));
                     let manifest = JSON.parse(zip.readAsText(entry));
+
+                    console.log('Getting file information...');
 
                     newVsixItem.item.title = `${manifest.packageName}`;
 
                     newVsixItem.item.description = `${manifest.packageDescription}`;
 
-                    // let itemInfo = {
-                    //     title:`${manifest.packageName}`,
-                    //     description: `${manifest.packageDescription}`
-                    // };
+                    newVsixItem.item.author = manifest.author;
 
-                    switch (options.type.toLowerCase()) {
-                        case "file":
-                            newVsixItem.item.link = `file://${options.baseurl}${file}`;
-                            break;
-                        case "http":
-                            newVsixItem.item.link= `http://${options.baseurl}${file}`;
-                            break;
-                        case "https":
-                            newVsixItem.item.link = `https://${options.baseurl}${file}`;
-                            break;
-                        default:
-                            console.log(chalk.red('Unknown type parameter...'));
-                            return;
+                    if ( options.baseurl.endsWith('/') )
+                    {
+                        newVsixItem.item.link = options.baseurl + file;
                     }
+                    else
+                    {
+                        newVsixItem.item.link = options.baseurl + '/' + file;       
+                    }
+                    console.log('Adding VSIX to the rss feed');
+                    
                     rssFeed._content.channel.push(newVsixItem);
-                    //let feedItem = vsixFeed.item(itemInfo); //vsixFeed.addItem(itemInfo);
                 }
                 
             });
         }
     });
-    // let a = toXml.toXML(rssFeed, xmlOptions);
-    // console.log(a);
-    // return;
 
     switch (options.exporttype.toLowerCase()) {
         case "json":
-            //fs.writeFileSync('./feed.json',vsixFeed.json1());
+            //need to implement
             break;
         case "rss":
-            fs.writeFileSync('./feed.xml',toXml.toXML(rssFeed, xmlOptions));
+            fs.writeFileSync('./feed.rss',toXml.toXML(rssFeed, xmlOptions));
+            console.log('Created rss feed');
+            var feedUrl = new url(options.baseurl);
+            
+            switch (feedUrl.protocol.toLowerCase()) 
+            {
+                case 'file:':
+                    console.log(chalk.green(`feed is available at ${feedUrl.protocol}//${feedUrl.host}/${path.resolve('feed.rss')}`));
+                    break;
+                case 'http:':
+                    console.log(chalk.green(`feed is available at ${feedUrl.protocol}//${feedUrl.host}/feed.rss`));
+                    break;
+                default:
+                    break;
+            }
             break;
         default:
             break;
     }
-    
 };
 
 cmdr.version('1.0.0')
 .command('create')
 .description('Creates a RSS feed xml based on a directory of .vsix files')
 .option('-d --directory <path>','Directory to parse where Citrix script packages are stored.')
-.option('-t --type <type>','Specifies the access type (FILE,HTTP')
-.option('-s --baseurl <base url>','Specifies the base FILE/HTTP URL for the VSIX files')
-.option('-e --exporttype [export type]','Specifies the export type, JSON,RSS,ATOM')
+.option('-b --baseurl <base url>','Specifies the base FILE/HTTP URL for the VSIX files')
+.option('-e --exporttype [export type]','Specifies the export type, RSS (currently)')
 .action(createFunction);
 
 cmdr.parse(process.argv);
